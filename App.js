f0e34600b93a2f6ce9c988fa23d28357715290cf
Arvin Lleva <arvin.lleva@newrocket.com>
2020-05-23 12:34:07 -0700

    Finished the basically styling layout for the button and input

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Test</Text>
+    <View style={{ padding: 50 }}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+          alignItems: "center",
+        }}
+      >
+        <TextInput
+          placeholder="Course Goal"
+          style={{
+            width: "80%",
+            borderBottomColor: "black",
+            borderBottomWidth: 1,
+            padding: 10,
+          }}
+        />
+        <Button title="Add" />
+      </View>
+      <View />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: {},
 });
